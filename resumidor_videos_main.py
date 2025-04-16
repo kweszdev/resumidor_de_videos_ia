82a34c0de015dd5819e537afeedb499f2cf81b24
kweszdev <kaueoliveira190409@gmail.com>
2025-04-16 05:06:58 -0300

    adição de tratamentos, divisao de carregar modelo(verificação) e transcrever

--- resumidor_videos_main.py
+++ resumidor_videos_main.py
@@ -19,9 +19,11 @@ oque cada função faz
 oque cada blibioteca faz
 avisar do subprocess
 win e unix
+os resumos sao salvos em txts
+
 -- melhorias
 windowns n linux
-dar opção de usar resumos antigos, como resumos aprofundados ou algo do tipo
+dar opção de usar resumos antigos, como resumos aprofundados ou algo do tipo em txts
 versões de programa, .exe com interface, terminal, etc - 
 tratamento
 nao so resumir videos resumir audios
@@ -33,10 +35,14 @@ verificar se ollama esta instlado e na porta certa, verificar se a ia escolhida 
 fazer fluxo figma
 tipos de prompt, nivel etc - ok
 inico da interface
+
 -- .exe
 --- dependencias
 empacotar com pyinstaller o binario do ffmpeg e usar o caminho relativo
 empacotar blibliotecas usadas 
+
+-- commit
+adição de tratamentos, divisao de carregar modelo(verificação) e transcrever
 """
 
 import whisper 
@@ -66,8 +72,11 @@ def main():
         # chamo o trancriber para trasncrever para txt
         transcriber_catch = transcriber(converter_catch)
 
+        # chamo para carregar modelo whispér
+        model_catch, model_cath = load_model()
+
         # chamo para resumir a trancrição do video
-        resumo = resume(transcriber_catch)
+        resumo = resume(transcriber_catch, model_catch)
 
         # verificando se o usuario quer um resummo mais aprofundado (resumo vire uma apostila)
         confirm = search_create(resumo)
@@ -78,8 +87,6 @@ def main():
 
     else:
         print("fechando software")
-
-
 
 # -- verificando se o ollama esta na maquina
 def verify_ollama():
@@ -148,56 +155,84 @@ def url_catcher():
 
 # -- converte o audio para mp3
 def converter(audio_yt):
-     # cria uma caminho para tranformar em mp4
-    audio_yt_path = audio_yt.download(filename='audio.mp4')
+    try:
+        # cria uma caminho para tranformar em mp4
+        audio_yt_path = audio_yt.download(filename='audio.mp4')
 
-    # converter para wav .16khz para mais nitidez
+        # converter para wav .16khz para mais nitidez
 
-    # converte para mp3
-    mp3_audio_path = 'audio.mp3'
-    audio_converter =  AudioFileClip(audio_yt_path) # varaivel para poder converter e add o path escolhido
-    print("\nConvetendo audio para mp3..")
-    audio_converter.write_audiofile(mp3_audio_path)
-    print("Audio convertido!")
-    return mp3_audio_path
+        # converte para mp3
+        mp3_audio_path = 'audio.mp3'
+        audio_converter =  AudioFileClip(audio_yt_path) # varaivel para poder converter e add o path escolhido
+        print("\nConvetendo audio para mp3..")
+        audio_converter.write_audiofile(mp3_audio_path)
+        print("Audio convertido!")
+        return mp3_audio_path
+    except Exception as error:
+        print(f"erro a converter, motivo: {error}")
+
+# -- carrega o modelo whisper
+def load_model():
+    global model_cath
+    try:
+        # verifica se ha modelo ja carregado 
+        if model_cath == "1":
+           btn = input(f"modelo {model_user} ja carregado, deseja carregar outro modelo?")
+           if btn == "":    
+            model_cath = 0
+            # repetição para escolher o modelo 
+            while not model_cath:
+                # escolhendo o medlo da ia whisper
+                model_user = input("""
+                Digite o modelo do Whisper a ser usado:
+
+                tiny    - Muito rápido, mas pouca precisão
+                base    - Rápido, mas ainda com erros
+                small   - Equilíbrio entre velocidade e precisão
+                medium  - Mais preciso, porém mais pesado
+                large   - Alta precisão (requer mais memória da GPU)
+
+                Modelo escolhido: 
+                """).strip().lower() # tratando de forma facil
+
+                # Verificando se o modelo é válido
+                modelos_validos = ["tiny", "base", "small", "medium", "large"]
+                if model_user not in modelos_validos:
+                    print("❌ Modelo inválido!")
+                else:
+                    # decidindo onde vai rodar o  modelo (se disponível)
+                    device = "cuda" if torch.cuda.is_available() else "cpu" # verifica se cuda(modelo do t=orch para compatibilidade, dependo da cpu) ou se nao houver usa a cpu 
+                    cpu_name = platform.processor()
+                    device_user = torch.cuda.get_device_name(0) if torch.cuda.is_available() else "{cpu_name}" # para pegar o nomme do presseasdor usado ou gpu
+                    print(f"\nCarregando modelo: '{model_user}' no dispositivo: {device_user}({device})...")
+
+                    # carregando modelo
+                    model = whisper.load_model(model_user).to(device)
+                    print("Modelo carregado com sucesso!\n")
+
+                    
+                    return [model, True]
+            else:
+                return [model, True]
+        else:
+            model_cath = 1
+            
+    except Exception as error:
+        print(f"erro ao carregar modelo, motivo: {error}")
 
 # -- converte e trancreve
-def transcriber(mp3_audio_path):
-    # escolhendo o medlo da ia whisper
-    model_user = input("""
-    Digite o modelo do Whisper a ser usado:
+def transcriber(mp3_audio_path, model):
+    try:
+        # trancrevendo o audio
+        print("\ntranscrevendo o audio...")
+        audio_transcribe = model.transcribe(mp3_audio_path, fp16=True, language="pt", verbose=True, temperature=0) # verbose mostra o pregresso, fp16 e para ela nao usar ou usar gpu , taparature para fazer o ehisper nao corrigir
+        print("audio trancrevido!")
 
-    tiny    - Muito rápido, mas pouca precisão
-    base    - Rápido, mas ainda com erros
-    small   - Equilíbrio entre velocidade e precisão
-    medium  - Mais preciso, porém mais pesado
-    large   - Alta precisão (requer mais memória da GPU)
-
-    Modelo escolhido: 
-    """).strip().lower() # tratando de forma facil
-
-    # Verificando se o modelo é válido
-    modelos_validos = ["tiny", "base", "small", "medium", "large"]
-    if model_user not in modelos_validos:
-        print("❌ Modelo inválido! Usando 'base' como padrão.")
-        model_user = "base"
-
-    # Carregando modelo com GPU (se disponível)
-    device = "cuda" if torch.cuda.is_available() else "cpu" # verifica se cuda(modelo do t=orch para compatibilidade, dependo da cpu) ou se nao houver usa a cpu 
-    cpu_name = platform.processor()
-    device_user = torch.cuda.get_device_name(0) if torch.cuda.is_available() else "{cpu_name}" # para pegar o nomme do presseasdor usado ou gpu
-    print(f"\nCarregando modelo: '{model_user}' no dispositivo: {device_user}({device})...")
-    model = whisper.load_model(model_user).to(device)
-    print("Modelo carregado com sucesso!\n")
-
-    # trancrevendo o audio
-    print("\ntranscrevendo o audio...")
-    audio_transcribe = model.transcribe(mp3_audio_path, fp16=True, language="pt", verbose=True, temperature=0) # verbose mostra o pregresso, fp16 e para ela nao usar ou usar gpu , taparature para fazer o ehisper nao corrigir
-    print("audio trancrevido!")
-
-    # pegando o teto da trancrisão
-    audio_txt = audio_transcribe['text']
-    return audio_txt
+        # pegando o teto da trancrisão
+        audio_txt = audio_transcribe['text']
+        return audio_txt
+    except Exception as error:
+        print("erro ao transcrever o audio!")
 
 # -- resume o video (atraves da comunicaçao com o ollama e a ia selecionada, api)
 def resume(audio_txt):
